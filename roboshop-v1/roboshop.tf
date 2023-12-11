d401cce789059ee1cce59e2f346d12781969074e
abdul <abdulsk054@gmail.com>
2023-12-11 12:03:15 +0530

    components configuration

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -8,10 +8,10 @@ resource "aws_instance" "frontend" {
   }
 }
 
-#resource "aws_route53_record" "frontend" {
-#  zone_id = aws_route53_zone.primary.zone_id
-#  name    = "www.example.com"
-#  type    = "A"
-#  ttl     = 300
-#  records = [aws_eip.lb.public_ip]
-#}
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z073136637APEOW2NQTH0"
+  name    = "frontend-dev.entertanova.com"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
