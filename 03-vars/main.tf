d954822542ceb9c41f83bd1b54185e595aee48fb
abdul <abdulsk054@gmail.com>
2023-12-12 11:39:22 +0530

    variables

--- 03-vars/main.tf
+++ 03-vars/main.tf
@@ -1,7 +1,35 @@
+# Plain Variable
 variable "fruit_name" {
   default = "apple"
 }
 
 output "fruit_name" {
   value = var.fruit_name
+}
+
+# List Variable
+variable "fruits" {
+  default = [ "apple", "banana" ]
+}
+
+# Map Variable
+variable "fruit_stock" {
+  default = {
+    apple  = 100
+    banana = 200
+  }
+}
+
+# Map of Map Variable
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 3
+    }
+    banana = {
+      stock = 200
+      price = 2
+    }
+  }
 }
