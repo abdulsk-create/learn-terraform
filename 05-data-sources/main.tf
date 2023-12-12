c4b69506663a55f8d60a1355b0961a03ade870a1
abdul <abdulsk054@gmail.com>
2023-12-12 12:35:51 +0530

    terraform data sources

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,5 +1,9 @@
-data "aws_ami" "example" {
+data "aws_ami_ids" "ami" {
   most_recent      = true
   name_regex       = "Centos-*"
   owners           = ["973714476881"]
+}
+
+output "ami" {
+  value = data.aws_ami_ids.ami
 }
