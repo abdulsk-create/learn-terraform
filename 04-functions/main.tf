b4a04829a7839cda98c903a1de94a6d213145c54
abdul <abdulsk054@gmail.com>
2023-12-12 12:19:57 +0530

    terraform functions

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -4,3 +4,13 @@ variable "fruits" {
 output "fruits" {
   value = element(var.fruits, 2)
 }
+
+variable "fruit_with_stock" {
+  default = {
+    apple = 100
+  }
+}
+
+output "fruit_stock" {
+  value = var.fruit_with_stock["banana"]
+}
