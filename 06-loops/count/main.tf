01dcc67f02ab3df09b1c35d46f6d0a7b0e542cd4
abdul <abdulsk054@gmail.com>
2023-12-13 11:44:15 +0530

    terraform loops

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,14 @@
+variable "components" {
+  default =  ["frontend", "mongodb" ]
+}
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = [ "sg-033bdcf77df8a24c1" ]
+
+  tags = {
+    Name = element(var.components, count.index)
+  }
+}
