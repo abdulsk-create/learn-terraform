55b3e5a030dd1dcfd1b614889fd07afe005caa80
abdul <abdulsk054@gmail.com>
2023-12-18 12:20:08 +0530

    terraform modules

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "instance" {
   tags = {
     Name = var.name
   }
+
 }
 
 resource "aws_route53_record" "record" {
@@ -14,6 +15,21 @@ resource "aws_route53_record" "record" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
+}
+
+resource "null" "ansible" {
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.entertanova.com, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
 }
 
 
