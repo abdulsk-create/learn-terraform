e7e788a097c6accc7facd6ebfa717262f200e020
abdul <abdulsk054@gmail.com>
2023-12-18 11:45:47 +0530

    terraform modules

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -10,6 +10,9 @@ output "ami" {
 
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
-  instance_type = "t3.small"
+  instance_type = var.instance_type
   vpc_security_group_ids = [ "sg-033bdcf77df8a24c1" ]
 }
+
+variable "instance_type" {}
+
